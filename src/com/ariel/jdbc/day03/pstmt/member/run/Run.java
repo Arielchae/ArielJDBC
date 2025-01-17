b37b28812eff1fbc9332ee01f75110f71845153e
user1 <user1@DESKTOP-23CGDIJ>
2025-01-17 11:08:54 +0900

    [ADD] JDBC 3일차 - PreparedStatement
    1. Statement를 사용하면 입력값을 받아서 실행하는 기능에 SQLInjection이 가능해짐.
    
    2. PreparedStatement를 사용하여 입력값 검증을 통함 SQLInjection 대비 가능
    
    3. PreparedStatement는 위치홀더를 사용하며 pstmt 객체 생성시 쿼리문이 필요하고 쿼리문을 실행할 땐 query
    변수를 전달하지 않음.
    
    4. Statement vs PreparedStatement
    
    4.1 Statement
    - createStatement() 메소드를 통해서 객체 생성
    - execute*()를 실행할 때 쿼리문이 필요함
    - 쿼리문을 별도로 컴파일 하지 않아서 단순 실행일 경우 빠름
    - ex) 전체정보조회
    
    4.2 PreparedStatement
    - Statement를 상속받아서 만들어진 인터페이스
    - prepareStatement() 메소들를 통해서 객체 생성하는데 이때 쿼리문 필요
    - 쿼리문을 미리 컴파일하여 캐싱한 후 재사용하는 구조
    - 쿼리문을 컴파일 할때 위치홀더(?)를 이용하여 값이 들어가는 부분을 표시한 후 쿼리문 실행전에 값을 셋팅해주어야함.
    - 컴파일 하는 과정이 있어 느릴 수 있지만 쿼리문을 반복해서 실행할 때는 속도가 빠름
    - 전달값이 있는 쿼리문에 대해서 SqlInjection을 방어할 수 있는 보안기능이 추가됨
    - ex) 아이디로 정보조회, 이름으로 정보조회
    2. PreparedStatement
    - Statement를 상속받아서 만들어진 인터페이스
    - prepareStatement() 메소들를 통해서 객체 생성하는데 이때 쿼리문 필요
    - 쿼리문을 미리 컴파일하여 캐싱한 후 재사용하는 구조
    - 쿼리문을 컴파일 할때 위치홀더(?)를 이용하여 값이 들어가는 부분을 표시한 후 쿼리문 실행전에 값을 셋팅해주어야함.
    - 컴파일 하는 과정이 있어 느릴 수 있지만 쿼리문을 반복해서 실행할 때는 속도가 빠름
    - 전달값이 있는 쿼리문에 대해서 SqlInjection을 방어할 수 있는 보안기능이 추가됨
    - ex) 아이디로 정보조회, 이름으로 정보조회

---
+++ src/com/ariel/jdbc/day03/pstmt/member/run/Run.java
@@ -0,0 +1,13 @@
+package com.ariel.jdbc.day03.pstmt.member.run;
+
+import com.ariel.pstmt.day03.member.view.MemberView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MemberView view = new MemberView();
+		view.startProgram();
+
+	}
+
+}
